43f03f25fd044286252c73e00f81a8536c089ef0
Jiar <jiar.world@gmail.com>
2017-01-03 22:35:44 +0800

    完成第一天、第二天任务。

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python3.5-webapp
 跟着”廖雪峰的python3网站开发实战教程“学网站开发。
+
+[学习地址](http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001432170876125c96f6cc10717484baea0c6da9bee2be4000)
---
+++ www/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding= utf-8 -*-
+
+# Created by jiar on 2017/1/3.
+# Github: https://github.com/Jiar/
+
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html', charset='UTF-8')
+
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
